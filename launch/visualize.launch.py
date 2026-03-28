9fd8c938341718903a10eaeebaa97b3081690452
PerFrivik <per.a.frivik@gmail.com>
2026-03-28 22:21:19 +0100

    added rviz viz launch

---
+++ launch/visualize.launch.py
@@ -0,0 +1,64 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration, Command
+from launch_ros.parameter_descriptions import ParameterValue
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    pkg_description = get_package_share_directory('a2_description')
+
+    default_urdf_path = os.path.join(pkg_description, 'urdf', 'a2.urdf')
+    default_rviz_config_path = os.path.join(pkg_description, 'rviz', 'default.rviz')
+
+    urdf_model_arg = DeclareLaunchArgument(
+        'urdf_model',
+        default_value=default_urdf_path,
+        description='Absolute path to robot urdf file'
+    )
+
+    rviz_config_arg = DeclareLaunchArgument(
+        'rviz_config',
+        default_value=default_rviz_config_path,
+        description='Absolute path to rviz config file'
+    )
+
+    robot_description = ParameterValue(
+        Command(['cat ', LaunchConfiguration('urdf_model')]),
+        value_type=str
+    )
+
+    robot_state_publisher_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        parameters=[{
+            'robot_description': robot_description,
+            'use_sim_time': False,
+        }]
+    )
+
+    # GUI slider window for interactive joint control
+    joint_state_publisher_gui_node = Node(
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui',
+        parameters=[{'use_sim_time': False}]
+    )
+
+    rviz2_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        output='screen',
+        arguments=['-d', LaunchConfiguration('rviz_config')],
+        parameters=[{'use_sim_time': False}]
+    )
+
+    return LaunchDescription([
+        urdf_model_arg,
+        rviz_config_arg,
+        robot_state_publisher_node,
+        joint_state_publisher_gui_node,
+        rviz2_node,
+    ])
